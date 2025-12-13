c3af385d42745726ab5eb72c3a8a3c7e6f03ca21
Thế Lực <theluc.1746@gmail.com>
2025-12-13 16:57:36 +0700

    Add ESLint config and check-file plugin to client/server
    Introduced ESLint Flat Config for both client and server, including the eslint-plugin-check-file for enforcing file and folder naming conventions. Updated package.json and lock files to include the new plugin, added .vscode/settings.json for workspace ESLint settings, and created a .gitignore for the server.

--- MERN/src/Icicle-V2/client/eslint.config.mjs
+++ MERN/src/Icicle-V2/client/eslint.config.mjs
@@ -1,15 +1,49 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import checkFile from "eslint-plugin-check-file";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx,js,jsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true, 
+        },
+      ],
+    },
+  },
+
+  {
+    files: ["src/components/**/*", "components/**/*"],
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{tsx,jsx}": "PASCAL_CASE",
+        },
+      ],
+    },
+  },
+
   globalIgnores([
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "tailwind.config.ts",
+    "postcss.config.mjs",
+    "eslint.config.mjs",
+    "src/app/**"
   ]),
 ]);
 
